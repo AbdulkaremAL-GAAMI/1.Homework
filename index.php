afb51b33aa9ad6cf5f56f4ab38c6fe3fb1037da7
Abdulkarem AL-GAAMI <abdulkarem712660845@gmail.com>
2021-07-15 19:40:33 +0300

    firt project

---
+++ index.php
@@ -0,0 +1,122 @@
+<?php include "files/header.php"; ?>
+        <ul>
+            <li>
+                <div class="product">
+                    <div id="pro_img">
+                        <a href="#"><img src="images/post.jpg" width="320" height="200px" alt=""></a>
+                    </div>
+                    <div id="pro_title">
+                        <a href="#">Product title</a>
+                    </div>
+                    <div id="pro_bay">
+                        <a href="#"><button>buy now</button></a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product">
+                    <div id="pro_img">
+                        <a href="#"><img src="images/post.jpg" width="320" height="200px" alt=""></a>
+                    </div>
+                    <div id="pro_title">
+                        <a href="#">Product title</a>
+                    </div>
+                    <div id="pro_bay">
+                        <a href="#"><button>buy now</button></a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product">
+                    <div id="pro_img">
+                        <a href="#"><img src="images/post.jpg" width="320" height="200px" alt=""></a>
+                    </div>
+                    <div id="pro_title">
+                        <a href="#">Product title</a>
+                    </div>
+                    <div id="pro_bay">
+                        <a href="#"><button>buy now</button></a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product">
+                    <div id="pro_img">
+                        <a href="#"><img src="images/post.jpg" width="320" height="200px" alt=""></a>
+                    </div>
+                    <div id="pro_title">
+                        <a href="#">Product title</a>
+                    </div>
+                    <div id="pro_bay">
+                        <a href="#"><button>buy now</button></a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product">
+                    <div id="pro_img">
+                        <a href="#"><img src="images/post.jpg" width="320" height="200px" alt=""></a>
+                    </div>
+                    <div id="pro_title">
+                        <a href="#">Product title</a>
+                    </div>
+                    <div id="pro_bay">
+                        <a href="#"><button>buy now</button></a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product">
+                    <div id="pro_img">
+                        <a href="#"><img src="images/post.jpg" width="320" height="200px" alt=""></a>
+                    </div>
+                    <div id="pro_title">
+                        <a href="#">Product title</a>
+                    </div>
+                    <div id="pro_bay">
+                        <a href="#"><button>buy now</button></a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product">
+                    <div id="pro_img">
+                        <a href="#"><img src="images/post.jpg" width="320" height="200px" alt=""></a>
+                    </div>
+                    <div id="pro_title">
+                        <a href="#">Product title</a>
+                    </div>
+                    <div id="pro_bay">
+                        <a href="#"><button>buy now</button></a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product">
+                    <div id="pro_img">
+                        <a href="#"><img src="images/post.jpg" width="320" height="200px" alt=""></a>
+                    </div>
+                    <div id="pro_title">
+                        <a href="#">Product title</a>
+                    </div>
+                    <div id="pro_bay">
+                        <a href="#"><button>buy now</button></a>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="product">
+                    <div id="pro_img">
+                        <a href="#"><img src="images/post.jpg" width="320" height="200px" alt=""></a>
+                    </div>
+                    <div id="pro_title">
+                        <a href="#">Product title</a>
+                    </div>
+                    <div id="pro_bay">
+                        <a href="#"><button>buy now</button></a>
+                    </div>
+                </div>
+            </li>
+            <div class="c"></div>
+        </ul>
+ <?php include "files/footer.php"; ?>
